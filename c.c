073f7b2b1256d0cb432e53497642e9a9c088f01f
Diomidis Spinellis <dds@aueb.gr>
2016-12-25 20:09:01 +0200

    Go from time_t to tm and back

--- c.c
+++ c.c
@@ -2,11 +2,17 @@
 #include <string.h>
 #include <time.h>
 
-/* Sleep for the specified number of milliseconds */
+#define SAMPLES_PER_SECOND 25
+#define SECONDS_TO_RECORD 120
+
+#define SAMPLES_TO_RECORD (SECONDS_TO_RECORD * SAMPLES_PER_SECOND)
+
+/* Compatbility functions */
 #if defined(unix)
 #include <sys/select.h>
 #include <sys/time.h>
 
+/* Sleep for the specified number of milliseconds */
 static void
 milli_sleep(long msec)
 {
@@ -20,6 +26,7 @@ milli_sleep(long msec)
 #include <windows.h>
 #include <stdint.h> // portable: uint64_t   MSVC: __int64
 
+/* Sleep for the specified number of milliseconds */
 static void
 milli_sleep(long msec)
 {
@@ -69,21 +76,34 @@ short_name(char *s)
 int
 main(int argc, char *argv[])
 {
-	char buff[100];
-	struct timeval now;
-	struct tm *t;
-	time_t tnow;
 	char *name;
+	int i;
 
 	name = short_name(argv[0]);
-	for (;;) {
+	for (i = 0; i < SAMPLES_TO_RECORD; i++) {
+		char human[100], normalized[100];
+		struct timeval now;
+		struct tm *tm;
+		time_t tnow, treverse;
+
 		gettimeofday(&now, NULL);
+
 		tnow = now.tv_sec;
-		t = gmtime(&tnow);
-		strftime(buff, sizeof(buff), "%Y-%m-%d %H:%M:%S", t);
-		printf("%llu.%llu\t%s\t%s\n", (unsigned long long)now.tv_sec,
-				(unsigned long long)now.tv_usec, name, buff);
+		if ((tm = gmtime(&tnow)) != NULL)
+			strftime(human, sizeof(human), "%Y-%m-%d %H:%M:%S", tm);
+		else
+			strcpy(human, "ERROR");
+
+		if ((time_t)(treverse = mktime(tm)) != -1)
+			strftime(normalized, sizeof(normalized),
+					"%Y-%m-%d %H:%M:%S", tm);
+		else
+			strcpy(normalized, "ERROR");
+		printf("%llu.%llu\t%s\t%s\t%llu\t%s\n",
+				(unsigned long long)now.tv_sec,
+				(unsigned long long)now.tv_usec, name, human,
+				(unsigned long long)treverse, normalized);
 		fflush(stdout);
-		milli_sleep(100);
+		milli_sleep(1000 / SAMPLES_PER_SECOND);
 	}
 }
