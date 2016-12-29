f6417880c02e876bc42fc663e34256ccfe9a2299
Diomidis Spinellis <dds@aueb.gr>
2016-12-29 16:40:42 +0200

    Sanitize and provide server as an argument

--- ntpdate.c
+++ ntpdate.c
@@ -15,101 +15,99 @@
  *
  */
 
+#include <err.h>
 #include <stdio.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
 
-main() {
-	ntpdate();
-}
+int
+main(int argc, char *argv[])
+{
+	char	*hostname = argv[1];
+	int	portno=123;		//NTP is port 123
+	int	maxlen=1024;		//check our buffers
+	int	i;			// misc var i
+	unsigned char msg[48]={010,0,0,0,0,0,0,0,0};	// the packet we send
+	unsigned long  buf[maxlen];	// the buffer we get back
+	//struct in_addr ipaddr;		//	
+	struct protoent *proto;		//
+	struct sockaddr_in server_addr;
+	int	s;	// socket
+	int	tmit;	// the time -- This is a time_t sort of
+	struct hostent *h;
 
-ntpdate() {
-char	*hostname="203.50.77.6";
-int	portno=123;		//NTP is port 123
-int	maxlen=1024;		//check our buffers
-int	i;			// misc var i
-unsigned char msg[48]={010,0,0,0,0,0,0,0,0};	// the packet we send
-unsigned long  buf[maxlen];	// the buffer we get back
-//struct in_addr ipaddr;		//	
-struct protoent *proto;		//
-struct sockaddr_in server_addr;
-int	s;	// socket
-int	tmit;	// the time -- This is a time_t sort of
+	//use Socket;
+	//
+	//#we use the system call to open a UDP socket
+	//socket(SOCKET, PF_INET, SOCK_DGRAM, getprotobyname("udp")) or die "socket: $!";
+	proto=getprotobyname("udp");
+	if ((s = socket(PF_INET, SOCK_DGRAM, proto->p_proto)) < 0)
+		err(1, "socket");
 
-//use Socket;
-//
-//#we use the system call to open a UDP socket
-//socket(SOCKET, PF_INET, SOCK_DGRAM, getprotobyname("udp")) or die "socket: $!";
-proto=getprotobyname("udp");
-s=socket(PF_INET, SOCK_DGRAM, proto->p_proto);
-if(s) {
-	//perror("asd");
-	//printf("socket=%d\n",s);
-}
-//
-//#convert hostname to ipaddress if needed
-//$ipaddr   = inet_aton($HOSTNAME);
-memset( &server_addr, 0, sizeof( server_addr ));
-server_addr.sin_family=AF_INET;
-server_addr.sin_addr.s_addr = inet_addr(hostname);
-//argv[1] );
-//i   = inet_aton(hostname,&server_addr.sin_addr);
-server_addr.sin_port=htons(portno);
-//printf("ipaddr (in hex): %x\n",server_addr.sin_addr);
+	if ((h = gethostbyname(argv[1])) == NULL)
+		err(1, "gethostbyname %s", argv[1]);
+	//#convert hostname to ipaddress if needed
+	//$ipaddr   = inet_aton($HOSTNAME);
+	memset( &server_addr, 0, sizeof( server_addr ));
+	server_addr.sin_family=AF_INET;
+	memcpy(&server_addr.sin_addr.s_addr, h->h_addr_list[0], sizeof(server_addr.sin_addr));
+	server_addr.sin_port=htons(portno);
+	//printf("ipaddr (in hex): %x\n",server_addr.sin_addr);
 
-/*
- * build a message.  Our message is all zeros except for a one in the
- * protocol version field
- * msg[] in binary is 00 001 000 00000000 
- * it should be a total of 48 bytes long
-*/
+	/*
+	 * build a message.  Our message is all zeros except for a one in the
+	 * protocol version field
+	 * msg[] in binary is 00 001 000 00000000 
+	 * it should be a total of 48 bytes long
+	*/
 
-// send the data
-i=sendto(s,msg,sizeof(msg),0,(struct sockaddr *)&server_addr,sizeof(server_addr));
+	// send the data
+	i=sendto(s,msg,sizeof(msg),0,(struct sockaddr *)&server_addr,sizeof(server_addr));
 
-// get the data back
-i=recv(s,buf,sizeof(buf),0);
-//printf("recvfr: %d\n",i);
-//perror("recvfr:");
+	// get the data back
+	i=recv(s,buf,sizeof(buf),0);
+	//printf("recvfr: %d\n",i);
+	//perror("recvfr:");
 
-//We get 12 long words back in Network order
-/*
-for(i=0;i<12;i++)
-	printf("%d\t%-8x\n",i,ntohl(buf[i]));
-*/
+	//We get 12 long words back in Network order
+	/*
+	for(i=0;i<12;i++)
+		printf("%d\t%-8x\n",i,ntohl(buf[i]));
+	*/
 
-/*
- * The high word of transmit time is the 10th word we get back
- * tmit is the time in seconds not accounting for network delays which
- * should be way less than a second if this is a local NTP server
- */
+	/*
+	 * The high word of transmit time is the 10th word we get back
+	 * tmit is the time in seconds not accounting for network delays which
+	 * should be way less than a second if this is a local NTP server
+	 */
 
-tmit=ntohl((time_t)buf[10]);	//# get transmit time
-//printf("tmit=%d\n",tmit);
+	tmit=ntohl((time_t)buf[10]);	//# get transmit time
+	//printf("tmit=%d\n",tmit);
 
-/*
- * Convert time to unix standard time NTP is number of seconds since 0000
- * UT on 1 January 1900 unix time is seconds since 0000 UT on 1 January
- * 1970 There has been a trend to add a 2 leap seconds every 3 years.
- * Leap seconds are only an issue the last second of the month in June and
- * December if you don't try to set the clock then it can be ignored but
- * this is importaint to people who coordinate times with GPS clock sources.
- */
+	/*
+	 * Convert time to unix standard time NTP is number of seconds since 0000
+	 * UT on 1 January 1900 unix time is seconds since 0000 UT on 1 January
+	 * 1970 There has been a trend to add a 2 leap seconds every 3 years.
+	 * Leap seconds are only an issue the last second of the month in June and
+	 * December if you don't try to set the clock then it can be ignored but
+	 * this is importaint to people who coordinate times with GPS clock sources.
+	 */
 
-tmit-= 2208988800U;	
-//printf("tmit=%d\n",tmit);
-/* use unix library function to show me the local time (it takes care
- * of timezone issues for both north and south of the equator and places
- * that do Summer time/ Daylight savings time.
- */
+	tmit-= 2208988800U;	
+	//printf("tmit=%d\n",tmit);
+	/* use unix library function to show me the local time (it takes care
+	 * of timezone issues for both north and south of the equator and places
+	 * that do Summer time/ Daylight savings time.
+	 */
 
 
-//#compare to system time
-printf("Time: %s",ctime(&tmit));
-i=time(0);
-//printf("%d-%d=%d\n",i,tmit,i-tmit);
-printf("System time is %d seconds off\n",i-tmit);
+	//#compare to system time
+	printf("Time: %s",ctime((time_t *)&tmit));
+	i=time(0);
+	//printf("%d-%d=%d\n",i,tmit,i-tmit);
+	printf("System time is %d seconds off\n",i-tmit);
 }
